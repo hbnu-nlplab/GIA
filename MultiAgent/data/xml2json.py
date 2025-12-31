990ee54513a71db8af6419fd18067a76b232d60d
hyunjung2222 <20222039@edu.hanbat.ac.kr>
2025-12-31 15:43:20 +0900

    실험 수정

--- MultiAgent/data/xml2json.py
+++ MultiAgent/data/xml2json.py
@@ -1,10 +1,14 @@
 import xmltodict
 import json
 
-with open("input.xml", "r", encoding="utf-8") as f:
+
+XML_FILE = "./original/xml/CE01.xml"
+OUTPUT_JSON = "./original/json/CE01.json"
+
+with open(XML_FILE, "r", encoding="utf-8") as f:
     xml_data = f.read()
 
 dict_data = xmltodict.parse(xml_data)
 
-with open("output.json", "w", encoding="utf-8") as f:
+with open(OUTPUT_JSON, "w", encoding="utf-8") as f:
     json.dump(dict_data, f, ensure_ascii=False, indent=2)
